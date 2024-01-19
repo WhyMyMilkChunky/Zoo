8d4126aeddaae911d5135bd48a28df27e39e80d9
BunnBunnAtGeorgian <145400484+BunnBunnAtGeorgian@users.noreply.github.com>
2024-01-19 12:49:22 -0500

    Added sheep

---
+++ Assets/Animal/Animal.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Animal : MonoBehaviour
+{
+    public int movementSpeed { get; private set; }
+
+}
