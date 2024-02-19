73974ac43855158b787cf2926ebab0353bf1cc84
Fightingmetal <145400380+Fightingmetal@users.noreply.github.com>
2024-02-18 20:38:00 -0500

    added cow milking

--- Assets/Animal/Cow.cs
+++ Assets/Animal/Cow.cs
@@ -1,10 +1,12 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using static Animal;
 
 public class Cow : Animal
 {
+    private bool isRotating = false;
+    private bool isInTriggerZone = false;
+    public GameObject bucket;
+
     void Start()
     {
         GetAnimalComponents();
@@ -13,6 +15,76 @@ public class Cow : Animal
 
     void Update()
     {
+        if (isInTriggerZone && Input.GetKeyDown(KeyCode.Space) && !isRotating)
+        {
+            Rotate();
+            milkBucket();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isInTriggerZone = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isInTriggerZone = false;
+        }
+    }
+
+    void Rotate()
+    {
+        StartCoroutine(RotationCoroutine());
+    }
+
+    IEnumerator RotationCoroutine()
+    {
+        isRotating = true;
+
+        Quaternion startRotation = transform.rotation;
+        Quaternion endRotation = Quaternion.Euler(-70f, 0f, 180f); 
+
+        float duration = 2f;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            transform.rotation = Quaternion.Lerp(startRotation, endRotation, elapsedTime / duration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.rotation = endRotation;
+
+        yield return new WaitForSeconds(0.5f);
+
+        startRotation = endRotation;
+        endRotation = Quaternion.Euler(-90f, 0f, 180f);
+
+        elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            transform.rotation = Quaternion.Lerp(startRotation, endRotation, elapsedTime / duration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.rotation = endRotation;
+
+        isRotating = false;
+    }
+
+    void milkBucket()
+    {
+        GameObject newObject = Instantiate(bucket, transform.position - new Vector3(2.5f, 3f, 1f), Quaternion.identity);
         
+        Destroy(newObject, 4f);
     }
 }
